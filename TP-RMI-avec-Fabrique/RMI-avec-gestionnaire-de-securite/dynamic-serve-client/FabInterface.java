52e2ecbb335799a5369e5ca76ed2e145aeb69125
nesrine-khiari <147988492+nesrine-khiari@users.noreply.github.com>
2024-05-21 22:39:51 +0100

    RMI avec fabrique

--- TP-RMI-avec-Fabrique/RMI-avec-gestionnaire-de-securite/dynamic-serve-client/FabInterface.java
+++
@@ -1,8 +0,0 @@
-package src.Fabrique;
-import src.Server.AppInterface;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface FabInterface extends Remote {
-    AppInterface newReverse() throws RemoteException;
-}
